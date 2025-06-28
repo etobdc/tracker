ab4db4d1894783ca498fe117bf70938ad750c632
Everton Dalla Corte <etodalla@gmail.com>
2025-06-28 19:32:14 -0300

    add new tracker form

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,63 @@
-import Image from "next/image";
+'use client'
+
+import {Button, Container, Grid, TextField} from '@mui/material';
+import Tracker from "@/components/tracker";
+import { useState } from 'react';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [trackerList, setTrackerList] = useState([])
+  const [title, setTitle] = useState('Vida')
+  const [measureUnit, setMeasureUnit] = useState('HP')
+  const [initialValue, setInitialValue] = useState(80)
+  const [minValue, setMinValue] = useState(0)
+  const [maxValue, setMaxValue] = useState(100)
+  const [stepValue, setStepValue] = useState(5)
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  function addTrackerToList() {
+    const newTracker = {
+      title,
+      measureUnit,
+      initialValue,
+      minValue,
+      maxValue,
+      stepValue
+    }
+    setTrackerList([...trackerList, newTracker])
+  }
+
+  return (
+    <Container maxWidth="sm" className='mt-5'>
+      <Grid container spacing={1}>
+        <Grid size={6}>
+          <TextField fullWidth variant="outlined" type="text" id="title" label="Tracker name" value={title}  onChange={(event) => setTitle(event.target.value)} />
+        </Grid>
+        <Grid size={6}>
+          <TextField fullWidth variant="outlined" type="text" id="measureUnit" label="Measure unit" value={measureUnit}  onChange={(event) => setMeasureUnit(event.target.value)} />
+        </Grid>
+        <Grid size={6}>
+          <TextField fullWidth variant="outlined" type="number" id="minValue" label="Minimum value" value={minValue} onChange={(event) => setMinValue(event.target.value)} />
+        </Grid>
+        <Grid size={6}>
+          <TextField fullWidth variant="outlined" type="number" id="maxValue" label="Maximum value" value={maxValue} onChange={(event) => setMaxValue(event.target.value)} />
+        </Grid>
+        <Grid size={6}>
+          <TextField fullWidth variant="outlined" type="number" id="initialValue" label="Initial value" value={initialValue} onChange={(event) => setInitialValue(event.target.value)} />
+        </Grid>
+        <Grid size={6}>
+          <TextField fullWidth variant="outlined" type="number" id="stepValue" label="Step value" value={stepValue} onChange={(event) => setStepValue(event.target.value)} />
+        </Grid>
+        <Grid size={12} justifyItems={'end'}>
+          <Button size='large' color='success' variant="contained" onClick={() => addTrackerToList()}>
+            Add New Tracker
+          </Button>
+        </Grid>
+        <Grid size={12} className="mt-5">
+          {trackerList.map((item, index) => (
+            <Tracker key={index} trackerProps={item} />
+          ))}
+        </Grid>
+      </Grid>
+     
+    </Container>
   );
 }
---
+++ src/components/tracker.js
@@ -0,0 +1,36 @@
+'use client'
+import {Paper, Slider} from '@mui/material';
+import { useState } from 'react';
+
+export default function Tracker({trackerProps}) {
+  const [value, setValue] = useState(trackerProps.initialValue)
+  
+  function valuetext(value) {
+    return `${value} ${trackerProps.measureUnit}`;
+  }
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Paper className='mb-5 px-5 py-5' elevation={5}>
+      <p className='text-center'>{trackerProps.title}</p>
+      <p className='text-center'>{value} {trackerProps.measureUnit}</p>
+      <Slider
+        aria-label={trackerProps.title}
+        defaultValue={trackerProps.initialValue}
+        value={value}
+        getAriaValueText={valuetext}
+        valueLabelDisplay="auto"
+        valueLabelFormat={valuetext}
+        shiftStep={trackerProps.stepValue}
+        step={trackerProps.stepValue}
+        marks
+        min={trackerProps.minValue}
+        max={trackerProps.maxValue}
+        onChange={handleChange}
+      />
+    </Paper>
+  );
+}
