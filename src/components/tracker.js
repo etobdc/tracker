c15bf5cdbaccf00f75fee60d80ec4f7071790408
Everton Dalla Corte <etodalla@gmail.com>
2025-06-28 20:09:46 -0300

    add trackers labels

--- src/components/tracker.js
+++ src/components/tracker.js
@@ -1,5 +1,5 @@
 'use client'
-import {Paper, Slider} from '@mui/material';
+import {Grid, Paper, Slider} from '@mui/material';
 import { useState } from 'react';
 
 export default function Tracker({trackerProps}) {
@@ -15,23 +15,43 @@ export default function Tracker({trackerProps}) {
 
   return (
     <Paper className='mb-5 px-5 py-5' elevation={5}>
-      <p className='text-center'>{trackerProps.title}</p>
-      <p className='text-center'>{value} {trackerProps.measureUnit}</p>
-      <Slider
-        aria-label={trackerProps.title}
-        defaultValue={trackerProps.initialValue}
-        value={value}
-        style={{color: trackerProps.color}}
-        getAriaValueText={valuetext}
-        valueLabelDisplay="auto"
-        valueLabelFormat={valuetext}
-        shiftStep={trackerProps.stepValue}
-        step={trackerProps.stepValue}
-        marks
-        min={trackerProps.minValue}
-        max={trackerProps.maxValue}
-        onChange={handleChange}
-      />
+      <Grid container spacing={1}>
+        <Grid size={6}>
+          <p className='text-center font-black'>{trackerProps.title}</p>
+        </Grid>
+        <Grid size={6}>
+          <p className='text-center font-black'>{value} {trackerProps.measureUnit}</p>
+        </Grid>
+        <Grid size={12}>
+          <Slider
+            aria-label={trackerProps.title}
+            defaultValue={trackerProps.initialValue}
+            value={value}
+            style={{color: trackerProps.color}}
+            getAriaValueText={valuetext}
+            valueLabelDisplay="auto"
+            valueLabelFormat={valuetext}
+            shiftStep={trackerProps.stepValue}
+            step={trackerProps.stepValue}
+            marks
+            min={trackerProps.minValue}
+            max={trackerProps.maxValue}
+            onChange={handleChange}
+          />
+        </Grid>
+      </Grid>
+        <Grid
+          container
+          direction="row"
+          sx={{
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <span>{trackerProps.minValue} {trackerProps.measureUnit}</span>
+          <span>{trackerProps.maxValue / 2} {trackerProps.measureUnit}</span>
+          <span>{trackerProps.maxValue} {trackerProps.measureUnit}</span>
+        </Grid>
     </Paper>
   );
 }
