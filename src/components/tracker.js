9f7b1ceea955596a5bd50279d41784acfbab9442
Everton Dalla Corte <etodalla@gmail.com>
2025-06-28 22:30:15 -0300

    add field to change value

--- src/components/tracker.js
+++ src/components/tracker.js
@@ -1,11 +1,12 @@
 'use client'
-import {Grid, IconButton, Paper, Slider} from '@mui/material';
+import {Button, Grid, IconButton, TextField, Paper, Slider} from '@mui/material';
 import { useState } from 'react';
-import CloseIcon from '@mui/icons-material/Close';
+import {Close, RemoveCircle, AddCircle } from '@mui/icons-material';
 
 
 export default function Tracker({trackerProps, remove}) {
   const [value, setValue] = useState(trackerProps.initialValue)
+  const [newValue, setNewValue] = useState(trackerProps.stepValue)
   
   function valuetext(value) {
     return `${value} ${trackerProps.measureUnit}`;
@@ -15,21 +16,54 @@ export default function Tracker({trackerProps, remove}) {
     setValue(newValue);
   };
 
+  const subtractValue = () => {
+    const result = value - newValue
+    setNewValue(trackerProps.stepValue)
+    setValue(result >= 0 ? result : 0)
+  };
+
+  const addValue = () => {
+    const result = value + newValue
+    setNewValue(trackerProps.stepValue)
+    setValue(result <= trackerProps.maxValue ? result : trackerProps.maxValue)
+  };
+
   return (
-    <Paper className='mb-5 px-5 py-5 relative' elevation={5}>
-      <div className="absolute right-1 top-1">
+    <Paper className='mb-5 relative' elevation={5}>
+      <div className="absolute right-0 top-0">
         <IconButton onClick={remove} color="error" aria-label="delete tracker">
-          <CloseIcon />
+          <Close />
         </IconButton>
       </div>
-      <Grid container spacing={1}>
-        <Grid size={6}>
-          <p className='text-center text-lg font-black'>{trackerProps.title}</p>
+      <Grid container spacing={0}>
+        <Grid size={5}>
+          <Grid container spacing={0}>
+            <Grid size={6} className="py-6 bg-emerald-400">
+              <p className='text-center text-md font-black'>{value} {trackerProps.measureUnit}</p>
+            </Grid>
+            <Grid size={6} className="py-6 bg-amber-400">
+              <p className='text-center text-md font-black'>{trackerProps.title}</p>
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid size={6}>
-          <p className='text-center text-lg font-black'>{value} {trackerProps.measureUnit}</p>
+        <Grid size={7} className="py-2 bg-amber-50">
+          <Grid container spacing={0} alignItems={'center'} className="h-full">
+            <Grid size={3} className="flex justify-end">
+              <Button variant='outlined' onClick={subtractValue} color="error" aria-label="delete tracker">
+                <RemoveCircle />
+              </Button>
+            </Grid>
+            <Grid size={5} className="px-2" justifyContent={'center'}>
+              <TextField fullWidth variant="outlined" type="number" id="newValue" label="Value" value={newValue} onChange={(event) => setNewValue(event.target.value)} />
+            </Grid>
+            <Grid size={3} className="flex justify-start">
+              <Button className="py-5" variant='outlined' onClick={addValue} color="success" aria-label="delete tracker">
+                <AddCircle />
+              </Button>
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid size={12}>
+        <Grid size={12} className="px-3 py-2">
           <Slider
             aria-label={trackerProps.title}
             defaultValue={trackerProps.initialValue}
@@ -45,20 +79,20 @@ export default function Tracker({trackerProps, remove}) {
             max={trackerProps.maxValue}
             onChange={handleChange}
           />
+          <Grid
+            container
+            direction="row"
+            sx={{
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <span>{trackerProps.minValue} {trackerProps.measureUnit}</span>
+            <span>{trackerProps.maxValue / 2} {trackerProps.measureUnit}</span>
+            <span>{trackerProps.maxValue} {trackerProps.measureUnit}</span>
+          </Grid>
         </Grid>
       </Grid>
-        <Grid
-          container
-          direction="row"
-          sx={{
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          <span>{trackerProps.minValue} {trackerProps.measureUnit}</span>
-          <span>{trackerProps.maxValue / 2} {trackerProps.measureUnit}</span>
-          <span>{trackerProps.maxValue} {trackerProps.measureUnit}</span>
-        </Grid>
     </Paper>
   );
 }
