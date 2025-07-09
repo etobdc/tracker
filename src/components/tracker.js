e7a5821fbdd9df8221c31da53d442dfdcccbd6c1
Everton Dalla Corte <etodalla@gmail.com>
2025-07-09 01:47:19 -0300

    Melhora layout dos trackers

--- src/components/tracker.js
+++ src/components/tracker.js
@@ -1,5 +1,5 @@
 'use client'
-import {Button, Grid, IconButton, TextField, Paper, Slider} from '@mui/material';
+import {Button, Grid, IconButton, TextField, Paper, Slider, Typography} from '@mui/material';
 import { useState } from 'react';
 import {Close, RemoveCircle, AddCircle } from '@mui/icons-material';
 
@@ -41,34 +41,34 @@ export default function Tracker({trackerProps, changeValue, remove}) {
         </IconButton>
       </div>
       <Grid container spacing={0}>
-        <Grid size={5}>
+        <Grid size={12}>
           <Grid container spacing={0}>
-            <Grid size={6} className="py-6 bg-emerald-400">
+            <Grid size={4} className="py-2 bg-emerald-400">
               <p className='text-center text-md font-black'>{value} {trackerProps.measureUnit}</p>
             </Grid>
-            <Grid size={6} className="py-6 bg-amber-400">
+            <Grid size={8} className="py-2 bg-amber-400">
               <p className='text-center text-md font-black'>{trackerProps.title}</p>
             </Grid>
           </Grid>
         </Grid>
-        <Grid size={7} className="py-2 bg-amber-50">
-          <Grid container spacing={0} alignItems={'center'} className="h-full">
-            <Grid size={3} className="flex justify-end">
+        <Grid size={12} className="pt-4">
+          <Grid container spacing={0} alignItems={'center'} justifyContent={'center'} className="h-full">
+            <Grid size={4} className="flex justify-end">
               <Button variant='outlined' onClick={subtractValue} color="error" aria-label="delete tracker">
                 <RemoveCircle />
               </Button>
             </Grid>
-            <Grid size={5} className="px-2" justifyContent={'center'}>
-              <TextField fullWidth variant="outlined" type="number" id="newValue" label="Value" value={newValue} onChange={(event) => setNewValue(event.target.value)} />
+            <Grid size={4} className="px-2" justifyContent={'center'}>
+              <TextField fullWidth size='small' variant="outlined" type="number" label="Value" value={newValue} onChange={(event) => setNewValue(event.target.value)} />
             </Grid>
-            <Grid size={3} className="flex justify-start">
+            <Grid size={4} className="flex justify-start">
               <Button className="py-5" variant='outlined' onClick={addValue} color="success" aria-label="delete tracker">
                 <AddCircle />
               </Button>
             </Grid>
           </Grid>
         </Grid>
-        <Grid size={12} className="px-3 py-2">
+        <Grid size={12} className="px-3 pb-1 relative">
           <Slider
             aria-label={trackerProps.title}
             defaultValue={trackerProps.value}
@@ -87,14 +87,21 @@ export default function Tracker({trackerProps, changeValue, remove}) {
           <Grid
             container
             direction="row"
+            className='absolute bottom-1 left-0 w-full px-3'
             sx={{
               justifyContent: "space-between",
               alignItems: "center",
             }}
           >
-            <span>{trackerProps.minValue} {trackerProps.measureUnit}</span>
-            <span>{trackerProps.maxValue / 2} {trackerProps.measureUnit}</span>
-            <span>{trackerProps.maxValue} {trackerProps.measureUnit}</span>
+            <Typography component={'span'} variant='caption'>
+              {trackerProps.minValue} {trackerProps.measureUnit}
+            </Typography>
+            <Typography component={'span'} variant='caption'>
+              {trackerProps.maxValue / 2} {trackerProps.measureUnit}
+            </Typography>
+            <Typography component={'span'} variant='caption'>
+              {trackerProps.maxValue} {trackerProps.measureUnit}
+            </Typography>
           </Grid>
         </Grid>
       </Grid>
